d3fb8ef3a88ed073bd2209299f12d46029bad839
jidapajueaiem <jidapajueaiem@gmail.com>
2026-01-23 17:32:00 +0700

    Updates

---
+++ precache-manifest.ca5e0a0bc43290fa445cf311b446eb82.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee9fe24e0a0c16543ab25d06499ff592",
+    "url": "/reactportfoliomg.jdp.don.look.this.if.HR.u.can.naa/index.html"
+  },
+  {
+    "revision": "e537c1bed231a44b2468",
+    "url": "/reactportfoliomg.jdp.don.look.this.if.HR.u.can.naa/static/css/2.58736d6f.chunk.css"
+  },
+  {
+    "revision": "f46dc0382f4b13a25fe3",
+    "url": "/reactportfoliomg.jdp.don.look.this.if.HR.u.can.naa/static/css/main.909d452a.chunk.css"
+  },
+  {
+    "revision": "e537c1bed231a44b2468",
+    "url": "/reactportfoliomg.jdp.don.look.this.if.HR.u.can.naa/static/js/2.dd3fc477.chunk.js"
+  },
+  {
+    "revision": "46a379a572333860f78765c743603e43",
+    "url": "/reactportfoliomg.jdp.don.look.this.if.HR.u.can.naa/static/js/2.dd3fc477.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f46dc0382f4b13a25fe3",
+    "url": "/reactportfoliomg.jdp.don.look.this.if.HR.u.can.naa/static/js/main.d8a6f8d9.chunk.js"
+  },
+  {
+    "revision": "8b0b37d4172989805d3c",
+    "url": "/reactportfoliomg.jdp.don.look.this.if.HR.u.can.naa/static/js/runtime-main.f07e40e6.js"
+  }
+]);
